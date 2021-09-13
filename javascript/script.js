c23c09878e4dc2ab383655f22f2980b5414988a1
mabunde <solomonsitati39@gmail.com>
2021-09-13 07:51:36 +0300

    Add toggle effect and form validation

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,33 @@
 $(document).ready(function(){
-    
+    $('#icon1').click(function(){
+        $('#p1').toggle('slow')
+        $('#icon1').slideUp();
+    });
+    $('#p1').click(function() {
+        $('#p1').hide()
+        $('icon1').show()
+    });
+
+    $('#icon2').click(function(){
+        $('#p2').toggle('slow')
+        $('#icon2').slideUp();
+    });
+    $('#icon3').click(function(){
+        $('#p3').toggle('slow')
+        $('#icon3').slideUp();
+    });
+    // form validation
+    $('form#contactForm').submit(function(event) {
+        var fName = $('input#inputName').val();
+        var eMail = $('input#inputEmail').val();
+        var message = $('textarea#inputMessage').val();
+
+        if($("input#inputName").val() && $("input#inputEmail").val()) {
+            alert(' Hi ' + fName + ' We have received your message. Thank you for reaching out to us');
+        }
+        else {
+            alert('Please fill in the form');
+        }
+        event.preventDefault();
+    })
 })
